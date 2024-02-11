2423a3fd6b61a311cae2f1ed5f42672ecfe22da4
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2024-02-12 02:11:59 +0300

    created ValidationMsgPatterns

---
+++ src/main/java/com/madtech/assessorai/utils/ValidationMsgPatterns.java
@@ -0,0 +1,10 @@
+package com.madtech.assessorai.utils;
+
+public class ValidationMsgPatterns {
+    private ValidationMsgPatterns() {}
+    public static final String notEmpty = "cannot be empty.";
+    public static final String notNull = "cannot be null.";
+    public static final String maxLenStr = "character count cannot be greater than";
+    public static final String minLenStr = "character count cannot be less than";
+    public static final String idCannotBeFound = "with id %d cannot be found.";
+}
